5a7090e14d05c751b48488b7d319c663481784e0
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-11-14 13:04:44 +0100

    doradivanje

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,6 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-import javafx.event.ActionEvent;
+/*import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -11,15 +11,20 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Color;*/
 
-import javax.swing.*;
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller{
 
-    @FXML public TextField input;
+    @FXML
+    public TextField input;
     @FXML public Button btn1;
     @FXML public Button btn2;
     @FXML public Button btn3;
@@ -39,9 +44,34 @@ public class Controller{
     @FXML public Button equalsBtn;
     @FXML public Label prom;
 
-    private long fnumber;
-    private long snumber;
+    private SimpleLongProperty fnumber;
+
+    public long getSnumber() {
+        return snumber.get();
+    }
+
+    public SimpleLongProperty snumberProperty() {
+        return snumber;
+    }
+
+    public void setSnumber(long snumber) {
+        this.snumber.set(snumber);
+    }
+
+    private SimpleLongProperty snumber;
     private String operation;
+
+    public long getFnumber() {
+        return fnumber.get();
+    }
+
+    public SimpleLongProperty fnumberProperty() {
+        return fnumber;
+    }
+
+    public void setFnumber(long fnumber) {
+        this.fnumber.set(fnumber);
+    }
 
     public void btn00(ActionEvent actionEvent){
         String staraVrijednost = input.getText();
@@ -111,7 +141,7 @@ public class Controller{
     public void sabiranje_click(ActionEvent actionEvent){
         String value = input.getText();
         long valueNumber = Integer.parseInt(value);
-        this.fnumber = valueNumber;
+        setFnumber(valueNumber);
         input.setText("");
         prom.setText(value + "+");
         operation = "+";
@@ -120,7 +150,7 @@ public class Controller{
     public void oduzimanje_click(ActionEvent actionEvent){
         String value = input.getText();
         long valueNumber = Integer.parseInt(value);
-        this.fnumber = valueNumber;
+        setFnumber(valueNumber);
         input.setText("");
         prom.setText(value + "-");
         operation = "-";
@@ -129,7 +159,7 @@ public class Controller{
     public void dijeljenje_click(ActionEvent actionEvent){
         String value = input.getText();
         long valueNumber = Integer.parseInt(value);
-        this.fnumber = valueNumber;
+        setFnumber(valueNumber);
         input.setText("");
         prom.setText(value + "/");
         operation = "/";
@@ -138,7 +168,7 @@ public class Controller{
     public void mnozenje_click(ActionEvent actionEvent){
         String value = input.getText();
         long valueNumber = Integer.parseInt(value);
-        this.fnumber = valueNumber;
+        setFnumber(valueNumber);
         input.setText("");
         prom.setText(value + "x");
         operation = "x";
@@ -147,7 +177,7 @@ public class Controller{
     public void mod_click(ActionEvent actionEvent){
         String value = input.getText();
         long valueNumber = Integer.parseInt(value);
-        this.fnumber = valueNumber;
+        setFnumber(valueNumber);
         input.setText("");
         prom.setText(value + "%");
         operation = "%";
@@ -157,8 +187,7 @@ public class Controller{
         switch(operation){
             case"+":
                 String value = input.getText();
-                this.snumber = Integer.parseInt(value);
-                long system = this.fnumber + this.snumber;
+                long system =getFnumber() + getSnumber();
                 input.setText(String.valueOf(system));
                 String oldProm = prom.getText();
                 prom.setText(oldProm + value);
@@ -166,8 +195,8 @@ public class Controller{
 
             case"-":
                 String valueOduzimanje = input.getText();
-                this.snumber = Integer.parseInt(valueOduzimanje);
-                long systemOduzimanje = this.fnumber + this.snumber;
+                setSnumber(Integer.parseInt(valueOduzimanje));
+                long systemOduzimanje = getFnumber() + getSnumber();
                 input.setText(String.valueOf(systemOduzimanje));
                 String oldPromOduzimanje = prom.getText();
                 prom.setText(oldPromOduzimanje + valueOduzimanje);
@@ -175,8 +204,8 @@ public class Controller{
 
             case"x":
                 String valueMnozenje = input.getText();
-                this.snumber = Integer.parseInt(valueMnozenje);
-                long systemMnozenje = this.fnumber * this.snumber;
+                setSnumber(Integer.parseInt(valueMnozenje));
+                long systemMnozenje = getFnumber() * getSnumber();
                 input.setText(String.valueOf(systemMnozenje));
                 String oldPromMnozenje = prom.getText();
                 prom.setText(oldPromMnozenje + valueMnozenje);
@@ -184,8 +213,8 @@ public class Controller{
 
             case "/":
                 String valueD = input.getText();
-                this.snumber = Integer.parseInt(valueD);
-                long systemD = this.fnumber / this.snumber;
+                setSnumber(Integer.parseInt(valueD));
+                long systemD = getFnumber() / getSnumber();
                 input.setText(String.valueOf(systemD));
                 String oldPromD = prom.getText();
                 prom.setText(oldPromD + valueD);
@@ -193,8 +222,8 @@ public class Controller{
 
             case "%":
                 String valueMod = input.getText();
-                this.snumber = Integer.parseInt(valueMod);
-                long systemMod = this.fnumber % this.snumber;
+                setSnumber(Integer.parseInt(valueMod));
+                long systemMod = getFnumber() % getSnumber();
                 input.setText(String.valueOf(systemMod));
                 String oldPromMod = prom.getText();
                 prom.setText(oldPromMod + valueMod);
