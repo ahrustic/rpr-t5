491015ffb163eb0638315bbab74c7a533eecfca1
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-11-15 09:52:46 +0100

    prepravke imena i metoda u kontroleru

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,31 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-/*import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;*/
-
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleLongProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class Controller{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
-    public TextField input;
-    @FXML public Button btn1;
+    public Label display;
+    /*@FXML public Button btn1;
     @FXML public Button btn2;
     @FXML public Button btn3;
     @FXML public Button btn4;
@@ -39,146 +30,171 @@ public class Controller{
     @FXML public Button oduzimanje;
     @FXML public Button plusBtn;
     @FXML public Button mnozenje;
-    @FXML public Button mod;
+    @FXML public Button modBtn;
     @FXML public Button dijeljenje;
-    @FXML public Button equalsBtn;
+    @FXML public Button equalsBtn;*/
     @FXML public Label prom;
 
-    private SimpleLongProperty fnumber;
+    private SimpleDoubleProperty fnumber = new SimpleDoubleProperty(0);
 
-    public long getSnumber() {
+    public double getSnumber() {
         return snumber.get();
     }
 
-    public SimpleLongProperty snumberProperty() {
+    public SimpleDoubleProperty snumberProperty() {
         return snumber;
     }
 
-    public void setSnumber(long snumber) {
+    public void setSnumber(double snumber) {
         this.snumber.set(snumber);
     }
 
-    private SimpleLongProperty snumber;
+    private SimpleDoubleProperty snumber = new SimpleDoubleProperty(0);
     private String operation;
 
-    public long getFnumber() {
+    public double getFnumber() {
         return fnumber.get();
     }
 
-    public SimpleLongProperty fnumberProperty() {
+    public SimpleDoubleProperty fnumberProperty() {
         return fnumber;
     }
 
-    public void setFnumber(long fnumber) {
+    public void setFnumber(double fnumber) {
         this.fnumber.set(fnumber);
     }
 
-    public void btn00(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn0(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
+        if (staraVrijednost == "0"){
         String set = "0";
-        input.setText(staraVrijednost + set);
+        display.setText(set);
+        }
+        else {
+            String set = "0";
+            display.setText(staraVrijednost + set);
+        }
     }
 
-    public void btn11(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn1(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
+        if(staraVrijednost == "0") {
         String set = "1";
-        input.setText(staraVrijednost + set);
+        display.setText(set);
+        }
+        else {
+            String set = "1";
+        display.setText(set);
+        }
     }
 
-    public void btn22(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
-        String set = "2";
-        input.setText(staraVrijednost + set);
+    public void btn2(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
+        if(staraVrijednost == "0") {
+            String set = "2";
+            display.setText(set);
+            display.setText(staraVrijednost + set);
+        }
+        else {
+            String set = "2";
+            display.setText(staraVrijednost + set);
+        }
     }
 
-    public void btn33(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn3(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "3";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void btn44(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn4(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "4";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void btn55(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn5(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "5";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void btn66(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn6(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "6";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void btn77(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn7(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "7";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void btn88(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
-        String set = "8";
-        input.setText(staraVrijednost + set);
+    public void btn8(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
+        if (staraVrijednost == "0"){
+            String set = "8";
+            display.setText(set);
+        }
+        else {
+            String set = "8";
+            display.setText(staraVrijednost + set);
+        }
     }
 
-    public void btn99(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void btn9(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = "9";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
 
-    public void zarez_click(ActionEvent actionEvent){
-        String staraVrijednost = input.getText();
+    public void dotBtn(ActionEvent actionEvent){
+        String staraVrijednost = display.getText();
         String set = ".";
-        input.setText(staraVrijednost + set);
+        display.setText(staraVrijednost + set);
     }
     public void sabiranje_click(ActionEvent actionEvent){
-        String value = input.getText();
-        long valueNumber = Integer.parseInt(value);
+        String value = display.getText();
+        double valueNumber = Double.parseDouble(value);
         setFnumber(valueNumber);
-        input.setText("");
+        display.setText("");
         prom.setText(value + "+");
         operation = "+";
     }
 
     public void oduzimanje_click(ActionEvent actionEvent){
-        String value = input.getText();
+        String value = display.getText();
         long valueNumber = Integer.parseInt(value);
         setFnumber(valueNumber);
-        input.setText("");
+        display.setText("");
         prom.setText(value + "-");
         operation = "-";
     }
 
     public void dijeljenje_click(ActionEvent actionEvent){
-        String value = input.getText();
+        String value = display.getText();
         long valueNumber = Integer.parseInt(value);
         setFnumber(valueNumber);
-        input.setText("");
+        display.setText("");
         prom.setText(value + "/");
         operation = "/";
     }
 
     public void mnozenje_click(ActionEvent actionEvent){
-        String value = input.getText();
+        String value = display.getText();
         long valueNumber = Integer.parseInt(value);
         setFnumber(valueNumber);
-        input.setText("");
+        display.setText("");
         prom.setText(value + "x");
         operation = "x";
     }
 
     public void mod_click(ActionEvent actionEvent){
-        String value = input.getText();
+        String value = display.getText();
         long valueNumber = Integer.parseInt(value);
         setFnumber(valueNumber);
-        input.setText("");
+        display.setText("");
         prom.setText(value + "%");
         operation = "%";
     }
@@ -186,45 +202,45 @@ public class Controller{
     public void jednako_click(ActionEvent actionEvent){
         switch(operation){
             case"+":
-                String value = input.getText();
-                long system =getFnumber() + getSnumber();
-                input.setText(String.valueOf(system));
+                String value = display.getText();
+                double system = getFnumber() + getSnumber();
+                display.setText(String.valueOf(system));
                 String oldProm = prom.getText();
                 prom.setText(oldProm + value);
                 break;
 
             case"-":
-                String valueOduzimanje = input.getText();
+                String valueOduzimanje = display.getText();
                 setSnumber(Integer.parseInt(valueOduzimanje));
-                long systemOduzimanje = getFnumber() + getSnumber();
-                input.setText(String.valueOf(systemOduzimanje));
+                double systemOduzimanje = getFnumber() + getSnumber();
+                display.setText(String.valueOf(systemOduzimanje));
                 String oldPromOduzimanje = prom.getText();
                 prom.setText(oldPromOduzimanje + valueOduzimanje);
                 break;
 
             case"x":
-                String valueMnozenje = input.getText();
+                String valueMnozenje = display.getText();
                 setSnumber(Integer.parseInt(valueMnozenje));
-                long systemMnozenje = getFnumber() * getSnumber();
-                input.setText(String.valueOf(systemMnozenje));
+                double systemMnozenje = getFnumber() * getSnumber();
+                display.setText(String.valueOf(systemMnozenje));
                 String oldPromMnozenje = prom.getText();
                 prom.setText(oldPromMnozenje + valueMnozenje);
                 break;
 
             case "/":
-                String valueD = input.getText();
+                String valueD = display.getText();
                 setSnumber(Integer.parseInt(valueD));
-                long systemD = getFnumber() / getSnumber();
-                input.setText(String.valueOf(systemD));
+                double systemD = getFnumber() / getSnumber();
+                display.setText(String.valueOf(systemD));
                 String oldPromD = prom.getText();
                 prom.setText(oldPromD + valueD);
                 break;
 
             case "%":
-                String valueMod = input.getText();
+                String valueMod = display.getText();
                 setSnumber(Integer.parseInt(valueMod));
-                long systemMod = getFnumber() % getSnumber();
-                input.setText(String.valueOf(systemMod));
+                double systemMod = getFnumber() % getSnumber();
+                display.setText(String.valueOf(systemMod));
                 String oldPromMod = prom.getText();
                 prom.setText(oldPromMod + valueMod);
                 break;
@@ -233,4 +249,8 @@ public class Controller{
         }
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        display.setText("0");
+    }
 }
